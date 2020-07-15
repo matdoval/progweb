fe871c090200de5e745c099ad8db6d6fba4d2f0a
matdoval <68256513+matdoval@users.noreply.github.com>
2020-07-15 14:27:30 -0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -9,7 +9,7 @@ function submitForm(){
 
     console.log(name)
 
-    saveMessageBackend(name, company, email, phone, message)
+    saveMessage(name, company, email, phone, message)
 
     afterSend();
     
@@ -31,26 +31,6 @@ function saveMessage(name, company, email, phone, message){
     )
 }
 
-function saveMessageBackend(name, company, email, phone, message){
-    // DatabaseReference
-    const addRequest = firebase.functions().httpsCallable("addRequest")
-    addRequest(
-	{
-	    name: name,
-	    company: company,
-	    email: email,
-	    phone: phone,
-	    message: message
-	}
-    ).then( () => {
-	alert("Mensagem enviada com sucesso");
-    }).catch( (error) => {
-	alert("Um erro ocorreu");
-    })
-}
-
-
-
 function afterSend(){
     confirm = document.getElementById("confirm");
     confirm.className = "open";
@@ -59,101 +39,6 @@ function afterSend(){
 	() => {
 	    confirm.className = "closed";
 	}, 3000);
-}
-
-var databaseReference = firebase.database().ref("TT905_2020")
-
-
-/*
-  Say Hello
-*/
-
-function sayHelloToFirebase() {
-    const form = document.forms['sayHello']
-    
-    var name     = form["name"].value
-
-    // Criando uma referência para a função que está no servidor.
-    const sayHello = firebase.functions().httpsCallable("sayHello");
-    sayHello(
-	{
-	    name : name 
-	}
-    ).then(
-	(result) => {
-	    alert(result.data);
-	}
-    );
-    
-    return false;
-}
-
-
-//////////////////////////////////////////////////
-//////////////////////////////////////////////////
-
-firebase.auth().onAuthStateChanged( (user) => {
-    header_form = document.getElementById("header_form");
-    logged_user = document.getElementById("logged_user");
-
-    if (user) {
-	header_form.className="closed"
-	logged_user.className="open"
-
-	user_span = document.getElementById("user_span")
-	user_span.innerHTML = user.email
-    } else {
-	header_form.className="open"
-	logged_user.className="closed"
-    }
-
-});
-
-function signOut(){        
-    firebase.auth().signOut().then(
-	() => {
-	    console.log('signedOut')
-	}
-    )
-}
-
-function submitAuthentication(e){
-    e.preventDefault();
-
-    console.log("ulisses");
-    
-    
-    const form = document.forms["login_form"];
-
-    const email = form.email.value;
-    const pass  = form.senha.value;
-
-    
-    firebase.auth().signInWithEmailAndPassword(email, pass).then(
-    	(user) => {
-	    form.reset()
-    	}
-    ).catch( (error) => {
-	alert(error.message);
-    });
-}
-
-
-function submitRegistro(e){
-    e.preventDefault();
-    const form = document.forms["login_form"];
-
-    const email = form.email.value;
-    const pass  = form.senha.value;
-
-    firebase.auth().createUserWithEmailAndPassword(email, pass).then(
-	(user) => {
-	    console.log('registered', user);
-	    form.reset();
-	}
-    ).catch( (error) => {
-	alert(error.message);
-    });
 }
 
 // Your web app's Firebase configuration
@@ -169,3 +54,4 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+var databaseReference = firebase.database().ref("TT905_2020")
